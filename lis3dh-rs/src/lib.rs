1eb7a936684690131cb748cfd9e1d14b1867c2ee
Alexis Marquet <alexis@alexismarquet.com>
2021-04-26 11:55:23 +0200

    adding lis3dh because we need the INT for moving that are not in the driver

---
+++ lis3dh-rs/src/lib.rs
@@ -0,0 +1,441 @@
+//! Platform-agnostic LIS3DH accelerometer driver which uses I²C via
+//! [embedded-hal]. This driver implements the [`Accelerometer`][acc-trait]
+//! and [`RawAccelerometer`][raw-trait] traits from the [accelerometer] crate.
+//!
+//! [embedded-hal]: https://docs.rs/embedded-hal
+//! [accelerometer]: https://docs.rs/accelerometer
+//! [acc-trait]: https://docs.rs/accelerometer/latest/accelerometer/trait.Accelerometer.html
+//! [raw-trait]: https://docs.rs/accelerometer/latest/accelerometer/trait.RawAccelerometer.html
+//!
+
+#![no_std]
+
+use core::convert::{TryFrom, TryInto};
+use core::fmt::Debug;
+
+pub use accelerometer;
+use accelerometer::error::Error as AccelerometerError;
+use accelerometer::vector::{F32x3, I16x3};
+use accelerometer::{Accelerometer, RawAccelerometer};
+use embedded_hal::blocking::i2c::{Write, WriteRead};
+
+mod register;
+use register::*;
+pub use register::{DataRate, DataStatus, Mode, Range, SlaveAddr};
+
+/// Accelerometer errors, generic around another error type `E` representing
+/// an (optional) cause of this error.
+#[derive(Debug)]
+pub enum Error<E> {
+    /// I²C bus error
+    I2C(E),
+
+    /// Invalid data rate selection
+    InvalidDataRate,
+
+    /// Invalid operating mode selection
+    InvalidMode,
+
+    /// Invalid full-scale selection
+    InvalidRange,
+
+    /// Attempted to write to a read-only register
+    WriteToReadOnly,
+
+    /// Invalid address provided
+    WrongAddress,
+}
+
+/// `LIS3DH` driver.
+pub struct Lis3dh<I2C> {
+    /// Underlying I²C device
+    i2c: I2C,
+
+    /// Current I²C slave address
+    address: u8,
+}
+
+impl<I2C, E> Lis3dh<I2C>
+where
+    I2C: WriteRead<Error = E> + Write<Error = E>,
+    E: Debug,
+{
+    /// Create a new LIS3DH driver from the given I2C peripheral. Default is
+    /// Hz_400 HighResolution.
+    pub fn new(i2c: I2C, address: SlaveAddr) -> Result<Self, Error<E>> {
+        let mut lis3dh = Lis3dh {
+            i2c,
+            address: address.addr(),
+        };
+
+        if lis3dh.get_device_id()? != DEVICE_ID {
+            return Err(Error::WrongAddress);
+        }
+
+        // Block data update
+        lis3dh.write_register(Register::CTRL4, BDU)?;
+
+        lis3dh.set_mode(Mode::HighResolution)?;
+
+        lis3dh.set_datarate(DataRate::Hz_400)?;
+
+        lis3dh.enable_axis((true, true, true))?;
+
+        // Enable ADCs.
+        lis3dh.write_register(Register::TEMP_CFG, ADC_EN)?;
+
+        Ok(lis3dh)
+    }
+
+    /// `WHO_AM_I` register.
+    pub fn get_device_id(&mut self) -> Result<u8, Error<E>> {
+        self.read_register(Register::WHOAMI)
+    }
+
+    /// X,Y,Z-axis enable.
+    /// `CTRL_REG1`: `Xen`, `Yen`, `Zen`
+    fn enable_axis(&mut self, (x, y, z): (bool, bool, bool)) -> Result<(), Error<E>> {
+        self.modify_register(Register::CTRL1, |mut ctrl1| {
+            ctrl1 &= !(X_EN | Y_EN | Z_EN); // disable all axes
+
+            ctrl1 |= if x { X_EN } else { 0 };
+            ctrl1 |= if y { Y_EN } else { 0 };
+            ctrl1 |= if z { Z_EN } else { 0 };
+
+            ctrl1
+        })
+    }
+
+    /// Operating mode selection.  
+    /// `CTRL_REG1`: `LPen` bit, `CTRL_REG4`: `HR` bit.  
+    /// You need to wait for stabilization after setting. In future this
+    /// function will be deprecated and instead take a `Delay` to do this for
+    /// you.
+    ///
+    /// | From           | To             | Wait for   |
+    /// |:---------------|:---------------|:-----------|
+    /// | HighResolution | LowPower       | 1/datarate |
+    /// | HighResolution | Normal         | 1/datarate |
+    /// | Normal         | LowPower       | 1/datarate |
+    /// | Normal         | HighResolution | 7/datarate |
+    /// | LowPower       | Normal         | 1/datarate |
+    /// | LowPower       | HighResolution | 7/datarate |
+    pub fn set_mode(&mut self, mode: Mode) -> Result<(), Error<E>> {
+        match mode {
+            Mode::LowPower => {
+                self.register_set_bits(Register::CTRL1, LP_EN)?;
+                self.register_clear_bits(Register::CTRL4, HR)?;
+            }
+            Mode::Normal => {
+                self.register_clear_bits(Register::CTRL1, LP_EN)?;
+                self.register_clear_bits(Register::CTRL4, HR)?;
+            }
+            Mode::HighResolution => {
+                self.register_clear_bits(Register::CTRL1, LP_EN)?;
+                self.register_set_bits(Register::CTRL4, HR)?;
+            }
+        }
+
+        Ok(())
+    }
+
+    /// Read the current operating mode.
+    pub fn get_mode(&mut self) -> Result<Mode, Error<E>> {
+        let ctrl1 = self.read_register(Register::CTRL1)?;
+        let ctrl4 = self.read_register(Register::CTRL4)?;
+
+        let is_lp_set = (ctrl1 >> 3) & 0x01 != 0;
+        let is_hr_set = (ctrl4 >> 3) & 0x01 != 0;
+
+        let mode = match (is_lp_set, is_hr_set) {
+            (true, false) => Mode::LowPower,
+            (false, false) => Mode::Normal,
+            (false, true) => Mode::HighResolution,
+            _ => return Err(Error::InvalidMode),
+        };
+
+        Ok(mode)
+    }
+
+    /// Data rate selection.
+    pub fn set_datarate(&mut self, datarate: DataRate) -> Result<(), Error<E>> {
+        self.modify_register(Register::CTRL1, |mut ctrl1| {
+            // Mask off lowest 4 bits
+            ctrl1 &= !ODR_MASK;
+            // Write in new output data rate to highest 4 bits
+            ctrl1 |= datarate.bits() << 4;
+
+            ctrl1
+        })
+    }
+
+    /// Read the current data selection rate.
+    pub fn get_datarate(&mut self) -> Result<DataRate, Error<E>> {
+        let ctrl1 = self.read_register(Register::CTRL1)?;
+        let odr = (ctrl1 >> 4) & 0x0F;
+
+        DataRate::try_from(odr).map_err(|_| Error::InvalidDataRate)
+    }
+
+    /// Full-scale selection.
+    pub fn set_range(&mut self, range: Range) -> Result<(), Error<E>> {
+        self.modify_register(Register::CTRL4, |mut ctrl4| {
+            // Mask off lowest 4 bits
+            ctrl4 &= !FS_MASK;
+            // Write in new full-scale to highest 4 bits
+            ctrl4 |= range.bits() << 4;
+
+            ctrl4
+        })
+    }
+
+    /// Read the current full-scale.
+    pub fn get_range(&mut self) -> Result<Range, Error<E>> {
+        let ctrl4 = self.read_register(Register::CTRL4)?;
+        let fs = (ctrl4 >> 4) & 0x03;
+
+        Range::try_from(fs).map_err(|_| Error::InvalidRange)
+    }
+
+    /// Set `REFERENCE` register.
+    pub fn set_ref(&mut self, reference: u8) -> Result<(), Error<E>> {
+        self.write_register(Register::REFERENCE, reference)
+    }
+
+    /// Read the `REFERENCE` register.
+    pub fn get_ref(&mut self) -> Result<u8, Error<E>> {
+        self.read_register(Register::REFERENCE)
+    }
+
+    /// Accelerometer data-available status.
+    pub fn get_status(&mut self) -> Result<DataStatus, Error<E>> {
+        let stat = self.read_register(Register::STATUS)?;
+
+        Ok(DataStatus {
+            zyxor: (stat & ZYXOR) != 0,
+            xyzor: ((stat & XOR) != 0, (stat & YOR) != 0, (stat & ZOR) != 0),
+            zyxda: (stat & ZYXDA) != 0,
+            xyzda: ((stat & XDA) != 0, (stat & YDA) != 0, (stat & ZDA) != 0),
+        })
+    }
+
+    /// Convenience function for `STATUS_REG` to confirm all three X, Y and
+    /// Z-axis have new data available for reading by accel_raw and associated
+    /// function calls.
+    pub fn is_data_ready(&mut self) -> Result<bool, Error<E>> {
+        let value = self.get_status()?;
+
+        Ok(value.zyxda)
+    }
+
+    /// Temperature sensor enable.
+    /// `TEMP_CGF_REG`: `TEMP_EN`, the BDU bit in `CTRL_REG4` is also set.
+    pub fn enable_temp(&mut self, enable: bool) -> Result<(), Error<E>> {
+        self.register_xset_bits(Register::TEMP_CFG, ADC_EN & TEMP_EN, enable)?;
+
+        // enable block data update (required for temp reading)
+        if enable {
+            self.register_xset_bits(Register::CTRL4, BDU, true)?;
+        }
+
+        Ok(())
+    }
+
+    /// Raw temperature sensor data as `i16`. The temperature sensor __must__
+    /// be enabled via `enable_temp` prior to reading.
+    pub fn get_temp_out(&mut self) -> Result<i16, Error<E>> {
+        let out_l = self.read_register(Register::OUT_ADC3_L)?;
+        let out_h = self.read_register(Register::OUT_ADC3_H)?;
+
+        Ok(i16::from_le_bytes([out_l, out_h]))
+    }
+
+    /// Temperature sensor data converted to `f32`. Output is in degree
+    /// celsius. The temperature sensor __must__ be enabled via `enable_temp`
+    /// prior to reading.
+    pub fn get_temp_outf(&mut self) -> Result<f32, Error<E>> {
+        let temp_out = self.get_temp_out()?;
+
+        Ok(temp_out as f32 / 256.0 + 25.0)
+    }
+
+    /// Configure interrupt 1
+    pub fn config_interrupt(&mut self) -> Result<(), Error<E>> {
+        // INT_1, DET_MOVE(1), 13, 2);
+        
+        self.write_register(Register::INT1_CFG, 0b0010_1010)?; // YHIE, XHIE
+        self.write_register(Register::INT1_THS, 100)?;
+        self.write_register(Register::INT1_DURATION, 2)?;
+        self.write_register(Register::CTRL3, I1_IA1)?; // I1_IA1
+        self.write_register(Register::CTRL6, 0)?;    // polarity active high
+        
+        // INT_2, DET_STOP(0), 13, 10);
+        /*
+        // interrupt pin configuration
+        self.write_register(Register::CTRL3, I1_IA1)?;
+
+        // enable interrupt 1 pin latched
+        self.write_register(Register::CTRL5, 1<< 3);
+        // configure threshold
+        self.write_register(Register::INT1_THS, 0x16)?;
+
+        // configure int duration
+        self.write_register(Register::INT1_DURATION, 0x02)?;
+
+
+        // configure movement detection with 3 axis high OR'ed
+        self.write_register(Register::INT1_CFG, IntEnable::CFG_ZHIE.bits() | IntEnable::CFG_YHIE.bits() | IntEnable::CFG_XHIE.bits())?;
+        */
+        Ok(())
+
+    }
+    pub fn get_int_source(&mut self) -> Result <u8, Error<E>> {
+        self.read_register(Register::INT1_SRC)
+    }
+    /// Modify a register's value. Read the current value of the register,
+    /// update the value with the provided function, and set the register to
+    /// the return value.
+    fn modify_register<F>(&mut self, register: Register, f: F) -> Result<(), Error<E>>
+    where
+        F: FnOnce(u8) -> u8,
+    {
+        let value = self.read_register(register)?;
+
+        self.write_register(register, f(value))
+    }
+
+    /// Clear the given bits in the given register.
+    fn register_clear_bits(&mut self, reg: Register, bits: u8) -> Result<(), Error<E>> {
+        self.modify_register(reg, |v| v & !bits)
+    }
+
+    /// Set the given bits in the given register.
+    fn register_set_bits(&mut self, reg: Register, bits: u8) -> Result<(), Error<E>> {
+        self.modify_register(reg, |v| v | bits)
+    }
+
+    /// Set or clear the given given bits in the given register, depending on
+    /// the value of `set`.
+    fn register_xset_bits(&mut self, reg: Register, bits: u8, set: bool) -> Result<(), Error<E>> {
+        if set {
+            self.register_set_bits(reg, bits)
+        } else {
+            self.register_clear_bits(reg, bits)
+        }
+    }
+
+    /// Read from the registers for each of the 3 axes.
+    fn read_accel_bytes(&mut self) -> Result<[u8; 6], Error<E>> {
+        let mut data = [0u8; 6];
+
+        self.i2c
+            .write_read(self.address, &[Register::OUT_X_L.addr() | 0x80], &mut data)
+            .map_err(Error::I2C)
+            .and(Ok(data))
+    }
+
+    /// Write a byte to the given register.
+    fn write_register(&mut self, register: Register, value: u8) -> Result<(), Error<E>> {
+        if register.read_only() {
+            return Err(Error::WriteToReadOnly);
+        }
+
+        self.i2c
+            .write(self.address, &[register.addr(), value])
+            .map_err(Error::I2C)
+    }
+
+    /// Read a byte from the given register.
+    fn read_register(&mut self, register: Register) -> Result<u8, Error<E>> {
+        let mut data = [0];
+
+        self.i2c
+            .write_read(self.address, &[register.addr()], &mut data)
+            .map_err(Error::I2C)
+            .and(Ok(data[0]))
+    }
+}
+
+impl<I2C, E> Accelerometer for Lis3dh<I2C>
+where
+    I2C: WriteRead<Error = E> + Write<Error = E>,
+    E: Debug,
+{
+    type Error = Error<E>;
+
+    /// Get normalized ±g reading from the accelerometer. You should be reading
+    /// based on data ready interrupt or if reading in a tight loop you should
+    /// waiting for `is_data_ready`.
+    fn accel_norm(&mut self) -> Result<F32x3, AccelerometerError<Self::Error>> {
+        // The official driver from ST was used as a reference.
+        // https://github.com/STMicroelectronics/STMems_Standard_C_drivers/tree/master/lis3dh_STdC
+        let mode = self.get_mode()?;
+        let range = self.get_range()?;
+
+        // See "2.1 Mechanical characteristics" in the datasheet to find the
+        // values below. Scale values have all been divided by 1000 in order
+        // to convert the resulting values from mG to G, while avoiding doing
+        // any actual division on the hardware.
+        let scale = match (mode, range) {
+            // High Resolution mode
+            (Mode::HighResolution, Range::G2) => 0.001,
+            (Mode::HighResolution, Range::G4) => 0.002,
+            (Mode::HighResolution, Range::G8) => 0.004,
+            (Mode::HighResolution, Range::G16) => 0.012,
+            // Normal mode
+            (Mode::Normal, Range::G2) => 0.004,
+            (Mode::Normal, Range::G4) => 0.008,
+            (Mode::Normal, Range::G8) => 0.016,
+            (Mode::Normal, Range::G16) => 0.048,
+            // Low Power mode
+            (Mode::LowPower, Range::G2) => 0.016,
+            (Mode::LowPower, Range::G4) => 0.032,
+            (Mode::LowPower, Range::G8) => 0.064,
+            (Mode::LowPower, Range::G16) => 0.192,
+        };
+
+        // Depending on which Mode we are operating in, the data has different
+        // resolution. Using this knowledge, we determine how many bits the
+        // data needs to be shifted. This is necessary because the raw data
+        // is in left-justified two's complement and we would like for it to be
+        // right-justified instead.
+        let shift: u8 = match mode {
+            Mode::HighResolution => 4, // High Resolution:  12-bit
+            Mode::Normal => 6,         // Normal:           10-bit
+            Mode::LowPower => 8,       // Low Power:         8-bit
+        };
+
+        let acc_raw = self.accel_raw()?;
+        let x = (acc_raw.x >> shift) as f32 * scale;
+        let y = (acc_raw.y >> shift) as f32 * scale;
+        let z = (acc_raw.z >> shift) as f32 * scale;
+
+        Ok(F32x3::new(x, y, z))
+    }
+
+    /// Get the sample rate of the accelerometer data.
+    fn sample_rate(&mut self) -> Result<f32, AccelerometerError<Self::Error>> {
+        Ok(self.get_datarate()?.sample_rate())
+    }
+}
+
+impl<I2C, E> RawAccelerometer<I16x3> for Lis3dh<I2C>
+where
+    I2C: WriteRead<Error = E> + Write<Error = E>,
+    E: Debug,
+{
+    type Error = Error<E>;
+
+    /// Get raw acceleration data from the accelerometer. You should be reading
+    /// based on data ready interrupt or if reading in a tight loop you should
+    /// waiting for `is_data_ready`.
+    fn accel_raw(&mut self) -> Result<I16x3, AccelerometerError<Self::Error>> {
+        let accel_bytes = self.read_accel_bytes()?;
+
+        let x = i16::from_le_bytes(accel_bytes[0..2].try_into().unwrap());
+        let y = i16::from_le_bytes(accel_bytes[2..4].try_into().unwrap());
+        let z = i16::from_le_bytes(accel_bytes[4..6].try_into().unwrap());
+
+        Ok(I16x3::new(x, y, z))
+    }
+}
---
+++ lis3dh-rs/src/register.rs
@@ -0,0 +1,264 @@
+use num_enum::TryFromPrimitive;
+
+/// Possible I²C slave addresses.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(u8)]
+pub enum SlaveAddr {
+    /// Default slave address (`0x18`)
+    Default = 0x18,
+
+    /// Alternate slave address (`0x19`)
+    Alternate = 0x19,
+}
+
+impl SlaveAddr {
+    pub fn addr(self) -> u8 {
+        return self as u8;
+    }
+}
+
+/// Enumerate all device registers.
+#[allow(dead_code, non_camel_case_types)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(u8)]
+pub enum Register {
+    STATUS_AUX = 0x07,
+    OUT_ADC1_L = 0x08,
+    OUT_ADC1_H = 0x09,
+    OUT_ADC2_L = 0x0A,
+    OUT_ADC2_H = 0x0B,
+    OUT_ADC3_L = 0x0C,
+    OUT_ADC3_H = 0x0D,
+    WHOAMI = 0x0F,
+    CTRL0 = 0x1E,
+    TEMP_CFG = 0x1F,
+    CTRL1 = 0x20,
+    CTRL2 = 0x21,
+    CTRL3 = 0x22,
+    CTRL4 = 0x23,
+    CTRL5 = 0x24,
+    CTRL6 = 0x25,
+    REFERENCE = 0x26,
+    STATUS = 0x27,
+    OUT_X_L = 0x28,
+    OUT_X_H = 0x29,
+    OUT_Y_L = 0x2A,
+    OUT_Y_H = 0x2B,
+    OUT_Z_L = 0x2C,
+    OUT_Z_H = 0x2D,
+    FIFO_CTRL = 0x2E,
+    FIFO_SRC = 0x2F,
+    INT1_CFG = 0x30,
+    INT1_SRC = 0x31,
+    INT1_THS = 0x32,
+    INT1_DURATION = 0x33,
+    INT2_CFG = 0x34,
+    INT2_SRC = 0x35,
+    INT2_THS = 0x36,
+    INT2_DURATION = 0x37,
+    CLICK_CFG = 0x38,
+    CLICK_SRC = 0x39,
+    CLICK_THS = 0x3A,
+    TIME_LIMIT = 0x3B,
+    TIME_LATENCY = 0x3C,
+    TIME_WINDOW = 0x3D,
+    ACT_THS = 0x3E,
+    ACT_DUR = 0x3F,
+}
+
+impl Register {
+    /// Get register address
+    pub fn addr(self) -> u8 {
+        self as u8
+    }
+
+    /// Is the register read-only?
+    pub fn read_only(self) -> bool {
+        match self {
+            Register::STATUS_AUX
+            | Register::OUT_ADC1_L
+            | Register::OUT_ADC1_H
+            | Register::OUT_ADC2_L
+            | Register::OUT_ADC2_H
+            | Register::OUT_ADC3_L
+            | Register::OUT_ADC3_H
+            | Register::WHOAMI
+            | Register::STATUS
+            | Register::OUT_X_L
+            | Register::OUT_X_H
+            | Register::OUT_Y_L
+            | Register::OUT_Y_H
+            | Register::OUT_Z_L
+            | Register::OUT_Z_H
+            | Register::FIFO_SRC
+            | Register::INT1_SRC
+            | Register::INT2_SRC
+            | Register::CLICK_SRC => true,
+            _ => false,
+        }
+    }
+}
+
+/// Full-scale selection.
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, TryFromPrimitive)]
+#[repr(u8)]
+pub enum Range {
+    /// ±16g
+    G16 = 0b11,
+
+    /// ±8g
+    G8 = 0b10,
+
+    /// ±4g
+    G4 = 0b01,
+
+    /// ±2g (Default)
+    G2 = 0b00,
+}
+
+impl Range {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+}
+
+/// Output data rate.
+#[allow(non_camel_case_types)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, TryFromPrimitive)]
+#[repr(u8)]
+pub enum DataRate {
+    /// 400Hz (Default)
+    Hz_400 = 0b0111,
+
+    /// 200Hz
+    Hz_200 = 0b0110,
+
+    /// 100Hz
+    Hz_100 = 0b0101,
+
+    /// 50Hz
+    Hz_50 = 0b0100,
+
+    /// 25Hz
+    Hz_25 = 0b0011,
+
+    /// 10Hz
+    Hz_10 = 0b0010,
+
+    /// 1Hz
+    Hz_1 = 0b0001,
+
+    /// Power down
+    PowerDown = 0b0000,
+}
+
+impl DataRate {
+    pub fn bits(self) -> u8 {
+        self as u8
+    }
+
+    pub fn sample_rate(self) -> f32 {
+        match self {
+            DataRate::Hz_400 => 400.0,
+            DataRate::Hz_200 => 200.0,
+            DataRate::Hz_100 => 100.0,
+            DataRate::Hz_50 => 50.0,
+            DataRate::Hz_25 => 25.0,
+            DataRate::Hz_10 => 10.0,
+            DataRate::Hz_1 => 1.0,
+            DataRate::PowerDown => 0.0,
+        }
+    }
+}
+
+/// Data status structure. Decoded from the `STATUS_REG` register.
+///
+/// `STATUS_REG` has the following bit fields:
+///   * `ZYXOR` - X, Y and Z-axis data overrun
+///   * `ZOR` - Z-axis data overrun
+///   * `YOR` - Y-axis data overrun
+///   * `XOR` - X-axis data overrun
+///   * `ZYXDA` - X, Y and Z-axis new data available
+///   * `ZDA` - Z-axis new data available
+///   * `YDA` Y-axis new data available
+///   * `XDA` X-axis new data available
+///
+/// This struct splits the fields into more convenient groups:
+///  * `zyxor` -> `ZYXOR`
+///  * `xyzor` -> (`XOR`, `YOR`, `ZOR`)
+///  * `zyxda` -> `ZYXDA`
+///  * `xyzda` -> (`XDA`, `YDA`, `ZDA`)
+#[derive(Debug)]
+pub struct DataStatus {
+    /// ZYXOR bit
+    pub zyxor: bool,
+
+    /// (XOR, YOR, ZOR) bits
+    pub xyzor: (bool, bool, bool),
+
+    /// ZYXDA bit
+    pub zyxda: bool,
+
+    /// (XDA, YDA, ZDA) bits
+    pub xyzda: (bool, bool, bool),
+}
+
+/// Operating mode.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(u8)]
+pub enum Mode {
+    /// High-resolution mode (12-bit data output)
+    HighResolution,
+
+    /// Normal mode (10-bit data output)
+    Normal,
+
+    /// Low-power mode (8-bit data output)
+    LowPower,
+}
+
+// === WHO_AMI_I (0Fh) ===
+
+/// `WHO_AM_I` device identification register
+pub const DEVICE_ID: u8 = 0x33;
+
+// === TEMP_CFG_REG (1Fh) ===
+
+pub const ADC_EN: u8 = 0b1000_0000;
+pub const TEMP_EN: u8 = 0b0100_0000;
+
+// === CTRL_REG1 (20h) ===
+
+pub const ODR_MASK: u8 = 0b1111_0000;
+pub const LP_EN: u8 = 0b0000_1000;
+pub const Z_EN: u8 = 0b0000_0100;
+pub const Y_EN: u8 = 0b0000_0010;
+pub const X_EN: u8 = 0b0000_0001;
+
+// === CTRL_REG3 (22h) ===
+
+pub const I1_CLICK: u8 = 0b1000_0000;
+pub const I1_IA1: u8 = 0b0100_0000;
+pub const I1_IA2: u8 = 0b0010_0000;
+pub const I1_ZYXDA: u8 = 0b0001_0000;
+pub const I1_321DA: u8 = 0b0000_1000;
+pub const I1_WTM: u8 = 0b0000_0100;
+pub const I1_OVERRUN: u8 = 0b0000_0010;
+
+// === CTRL_REG4 (23h) ===
+
+pub const BDU: u8 = 0b1000_0000;
+pub const FS_MASK: u8 = 0b0011_0000;
+pub const HR: u8 = 0b0000_1000;
+
+// === STATUS_REG (27h) ===
+
+pub const ZYXOR: u8 = 0b1000_0000;
+pub const ZOR: u8 = 0b0100_0000;
+pub const YOR: u8 = 0b0010_0000;
+pub const XOR: u8 = 0b0001_0000;
+pub const ZYXDA: u8 = 0b0000_1000;
+pub const ZDA: u8 = 0b0000_0100;
+pub const YDA: u8 = 0b0000_0010;
+pub const XDA: u8 = 0b0000_0001;
