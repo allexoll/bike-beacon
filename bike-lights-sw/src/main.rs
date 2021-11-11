b1f9dbd24df6806a0c1d07b12080a4090ad30dd0
Alexis Marquet <alexis@alexismarquet.com>
2021-11-11 09:42:55 +0100

    added conf guards for sleep and rtt, changed detection of button press/release, removed datetime because useless. addresses #1 . bumped hal to v0.8.0 and updated rtc API use. added readme

--- bike-lights-sw/src/main.rs
+++ bike-lights-sw/src/main.rs
@@ -2,31 +2,28 @@
 #![no_main]
 
 // pick a panicking behavior
-use core::panic::PanicInfo;
 use core::cell::{Cell, RefCell};
 use core::ops::DerefMut;
-
+use core::panic::PanicInfo;
 
 use cortex_m::interrupt::Mutex;
 use cortex_m::peripheral::NVIC;
 use cortex_m_rt::entry;
 
-use lis3dh::{Lis3dh, SlaveAddr, Mode};
-//use rtt_target::{rprintln, rtt_init_print};
+use lis3dh::{Lis3dh, Mode, SlaveAddr};
+use rtt_target::{rprintln, rtt_init_print};
 use stm32l0xx_hal::{
     exti::{ConfigurableLine, Exti, ExtiLine, GpioLine, TriggerEdge},
+    gpio::*,
     pac::{self, interrupt, Interrupt},
     prelude::*,
-    pwr::{PWR},
-    rcc::{Config},
+    pwr::PWR,
+    rcc::Config,
+    rtc::{self, Rtc},
     syscfg::SYSCFG,
-    rtc::{self, Instant, RTC},
     //lptim::{self, ClockSrc, LpTimer},
     timer::Timer,
-    gpio::*,
 };
-
-use build_timestamp::build_time;
 
 // light pattern
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -50,29 +47,32 @@ enum ButtonState {
     ClickedLong,
 }
 
-
 // shared values beetween main context and interrupts
 static LIS3DH_INT1_INT: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
 
 static BTN_STATE: Mutex<Cell<ButtonState>> = Mutex::new(Cell::new(ButtonState::Released));
-static BTN_GPIO: Mutex<RefCell<Option<gpioa::PA3<Input<Floating>>>>> = Mutex::new(RefCell::new(None));
+static BTN_GPIO: Mutex<RefCell<Option<gpioa::PA3<Input<Floating>>>>> =
+    Mutex::new(RefCell::new(None));
 
 static WAKEUP_INT: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
 
 static TIMER: Mutex<RefCell<Option<Timer<pac::TIM2>>>> = Mutex::new(RefCell::new(None));
 static PERIODIC: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
 static TICKS: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
+static PRESS_DURATION: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
+static PRESSED: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
 
 #[entry]
 fn main() -> ! {
-    //rtt_init_print!();
-    //rprintln!("bike-lights-sw");
+    #[cfg(debug_assertions)]
+    rtt_init_print!();
+    #[cfg(debug_assertions)]
+    rprintln!("bike-lights-sw");
 
     let cp = pac::CorePeripherals::take().unwrap();
     let dp = pac::Peripherals::take().unwrap();
 
     let mut rcc = dp.RCC.freeze(Config::hsi16());
-
 
     let gpioa = dp.GPIOA.split(&mut rcc);
     let gpiob = dp.GPIOB.split(&mut rcc);
@@ -81,23 +81,13 @@ fn main() -> ! {
     //let mut delay = cp.SYST.delay(rcc.clocks);
     let mut scb = cp.SCB;
 
-    // set RTC to build time
-    build_time!("%y:%m:%d:%H:%M:%S");
-    let instant = Instant::new()
-    .set_year(BUILD_TIME[0..2].parse::<u8>().unwrap())
-    .set_month(BUILD_TIME[3..5].parse::<u8>().unwrap())
-    .set_day(BUILD_TIME[6..8].parse::<u8>().unwrap())
-    .set_hour(BUILD_TIME[9..11].parse::<u8>().unwrap())
-    .set_minute(BUILD_TIME[12..14].parse::<u8>().unwrap())
-    .set_second(BUILD_TIME[15..17].parse::<u8>().unwrap());
-    let mut rtc = RTC::new(dp.RTC, &mut rcc, &mut pwr, instant);
+    let mut rtc = Rtc::new(dp.RTC, &mut rcc, &mut pwr, None).unwrap();
 
     rtc.enable_interrupts(rtc::Interrupts {
         wakeup_timer: true,
         ..rtc::Interrupts::default()
     });
     exti.listen_configurable(ConfigurableLine::RtcWakeup, TriggerEdge::Rising);
-
 
     let mut pm_5v_en = gpiob.pb1.into_push_pull_output();
     let acc_int_1 = gpioa.pa1.into_floating_input();
@@ -110,9 +100,7 @@ fn main() -> ! {
 
     let sda = gpiob.pb7.into_open_drain_output();
     let scl = gpiob.pb6.into_open_drain_output();
-    let mut i2c = dp.I2C1.i2c(sda, scl, 100.khz(), &mut rcc);
-
-    
+    let mut i2c = dp.I2C1.i2c(sda, scl, 100_000.Hz(), &mut rcc);
 
     let tube_btn = gpioa.pa3.into_floating_input();
     let line_tube_btn = GpioLine::from_raw_line(tube_btn.pin_number()).unwrap();
@@ -131,19 +119,18 @@ fn main() -> ! {
     pm_acc_pwr_en.set_high().unwrap();
 
     // ls3dh doesnt ack on first transfer, but does after... dont know why
-    let mut buffer:[u8;1] = [0];
-    let _ =  i2c.read(SlaveAddr::Alternate.addr(), &mut buffer);
+    let mut buffer: [u8; 1] = [0];
+    let _ = i2c.read(SlaveAddr::Alternate.addr(), &mut buffer);
     let mut lis3dh = Lis3dh::new(i2c, SlaveAddr::Alternate).unwrap();
-    
-    let mut timer = dp.TIM2.timer(20.hz(), &mut rcc);
-    timer.listen();
 
+    let mut timer = dp.TIM2.timer(20.Hz(), &mut rcc);
+    timer.listen();
 
     exti.listen_gpio(
         &mut syscfg,
         acc_int_1.port(),
         line_acc_int_1,
-        TriggerEdge::Rising,
+        TriggerEdge::Both,
     );
 
     exti.listen_gpio(
@@ -152,7 +139,6 @@ fn main() -> ! {
         line_tube_btn,
         TriggerEdge::Both,
     );
-
 
     let mut tube_state = TubeState::SolidOn;
     let mut tube_on_off = TubeOnOff::Off;
@@ -166,16 +152,15 @@ fn main() -> ! {
         NVIC::unmask(Interrupt::RTC);
         NVIC::unmask(Interrupt::TIM2);
     }
-    
+
     lis3dh.set_mode(Mode::LowPower).unwrap();
     // custom function to enable moving irq if acc > 1g any direction
     lis3dh.config_interrupt().unwrap();
 
-
     let mut snake_counter = 0;
     let mut blinker_counter = 0;
+    let mut old_pressed = false;
     loop {
-
         //pwr.enter_low_power_run_mode(rcc.clocks);
         /*pwr.stop_mode(
             &mut scb,
@@ -184,106 +169,113 @@ fn main() -> ! {
                 ultra_low_power: true,
             }
         ).enter();*/
+
+        #[cfg(not(debug_assertions))]
         pwr.sleep_mode(&mut scb).enter();
         //cortex_m::asm::wfi();
         //pwr.exit_low_power_run_mode();
- 
 
         let mut movement_has_happened = false;
         let mut timeout_has_happened = false;
         let mut periodic_has_happened = false;
         let mut button_has_happened = ButtonState::Released;
-        cortex_m::interrupt::free(|cs|  {
-                movement_has_happened = LIS3DH_INT1_INT.borrow(cs).get();
-                if movement_has_happened {
-                    LIS3DH_INT1_INT.borrow(cs).set(false);
-                }
-
-                timeout_has_happened = WAKEUP_INT.borrow(cs).get();
-                if timeout_has_happened {
-                    WAKEUP_INT.borrow(cs).set(false);
-                }
-
-                periodic_has_happened = PERIODIC.borrow(cs).get();
-                if periodic_has_happened {
-                    PERIODIC.borrow(cs).set(false);
-                }
-                button_has_happened = BTN_STATE.borrow(cs).get();
-                BTN_STATE.borrow(cs).set(ButtonState::Released);
+        cortex_m::interrupt::free(|cs| {
+            movement_has_happened = LIS3DH_INT1_INT.borrow(cs).get();
+            if movement_has_happened {
+                LIS3DH_INT1_INT.borrow(cs).set(false);
             }
-        );
 
-        if button_has_happened != ButtonState::Released
-        {
-            //rprintln!("btn: {:?}", button_has_happened);
+            timeout_has_happened = WAKEUP_INT.borrow(cs).get();
+            if timeout_has_happened {
+                WAKEUP_INT.borrow(cs).set(false);
+            }
+
+            periodic_has_happened = PERIODIC.borrow(cs).get();
+            if periodic_has_happened {
+                PERIODIC.borrow(cs).set(false);
+            }
+            let mut new_pressed = PRESSED.borrow(cs).get();
+            let press_duration = PRESS_DURATION.borrow(cs).get();
+            if new_pressed != old_pressed || press_duration > 10 {
+                if press_duration > 10 {
+                    button_has_happened = ButtonState::ClickedLong;
+                    PRESS_DURATION.borrow(cs).set(0);
+                    PRESSED.borrow(cs).set(false);
+                    new_pressed = false;
+                } else if !new_pressed {
+                    button_has_happened = ButtonState::ClickedShort;
+                }
+            }
+            old_pressed = new_pressed;
+        });
+
+        if button_has_happened != ButtonState::Released {
+            #[cfg(debug_assertions)]
+            rprintln!("btn: {:?}", button_has_happened);
             if button_has_happened == ButtonState::ClickedShort {
                 if tube_on_off == TubeOnOff::Off {
                     tube_on_off = TubeOnOff::On;
-                    pm_5v_en.set_high().unwrap();                        
+                    pm_5v_en.set_high().unwrap();
                     blinker_counter = 0;
-                    snake_counter = 0;  
-                }
-                else{
+                    snake_counter = 0;
+                } else {
                     tube_state = match tube_state {
                         TubeState::SolidOn => {
                             blinker_counter = 0;
                             TubeState::Blinking5Hz
-                        }, 
+                        }
                         TubeState::Blinking5Hz => {
                             snake_counter = 0;
                             TubeState::Snake
-                        },
-                        TubeState::Snake=> {
-                            TubeState::SolidOn 
                         }
+                        TubeState::Snake => TubeState::SolidOn,
                     };
                 }
             }
             if button_has_happened == ButtonState::ClickedLong {
                 tube_on_off = TubeOnOff::Off;
-                pm_5v_en.set_low().unwrap(); 
+                pm_5v_en.set_low().unwrap();
                 for i in leds.iter_mut() {
                     i.set_low().unwrap();
                 }
             }
-            //rprintln!("pressed: => {:?}:{:?}",tube_on_off, tube_state);
+            #[cfg(debug_assertions)]
+            rprintln!("pressed: => {:?}:{:?}", tube_on_off, tube_state);
         }
 
-        if movement_has_happened
-        {
-            //rprintln!("moved");
+        if movement_has_happened {
+            #[cfg(debug_assertions)]
+            rprintln!("moved");
             lis3dh.get_int_source().unwrap();
-            rtc.wakeup_timer().cancel();
+            rtc.wakeup_timer().cancel().unwrap();
             rtc.wakeup_timer().start(240u32);
         }
         if timeout_has_happened {
-            //rprintln!("timeout");
+            #[cfg(debug_assertions)]
+            rprintln!("timeout");
             tube_on_off = TubeOnOff::Off;
-            pm_5v_en.set_low().unwrap(); 
+            pm_5v_en.set_low().unwrap();
             for i in leds.iter_mut() {
                 i.set_low().unwrap();
             }
-
         }
         if periodic_has_happened {
-            if tube_on_off == TubeOnOff::On
-            {
+            if tube_on_off == TubeOnOff::On {
                 match tube_state {
                     TubeState::SolidOn => {
                         front_pwr_en.set_high().unwrap();
                         for i in leds.iter_mut() {
                             i.set_high().unwrap();
                         }
-                    },
+                    }
                     TubeState::Blinking5Hz => {
                         blinker_counter += 1;
-                        if blinker_counter %10 == 0 {
+                        if blinker_counter % 10 == 0 {
                             front_pwr_en.set_high().unwrap();
                             for i in leds.iter_mut() {
                                 i.set_high().unwrap();
                             }
-                        }
-                        else {
+                        } else {
                             front_pwr_en.set_low().unwrap();
                             for i in leds.iter_mut() {
                                 i.set_low().unwrap();
@@ -291,13 +283,12 @@ fn main() -> ! {
                         }
                     }
                     TubeState::Snake => {
-                        snake_counter +=1;
+                        snake_counter += 1;
                         front_pwr_en.toggle().unwrap();
                         for i in 0..=4 {
-                            if (snake_counter/2)%5 == i {
+                            if (snake_counter / 2) % 5 == i {
                                 leds[i].set_high().unwrap();
-                            }
-                            else{
+                            } else {
                                 leds[i].set_low().unwrap();
                             }
                         }
@@ -311,13 +302,13 @@ fn main() -> ! {
 #[inline(never)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    //rprintln!("{}", info);
+    #[cfg(debug_assertions)]
+    rprintln!("{}", info);
     loop {} // You might need a compiler fence in here.
 }
 
 #[interrupt]
-fn EXTI0_1()
-{
+fn EXTI0_1() {
     cortex_m::interrupt::free(|cs| {
         Exti::unpend(GpioLine::from_raw_line(1).unwrap());
         // ask mainloop to clear lis3dh interrupt source
@@ -326,28 +317,21 @@ fn EXTI0_1()
 }
 
 #[interrupt]
-fn EXTI2_3()
-{
+fn EXTI2_3() {
     cortex_m::interrupt::free(|cs| {
         if let Some(ref mut btn) = BTN_GPIO.borrow(cs).borrow_mut().deref_mut() {
-            if btn.is_high().unwrap(){ // release button
-                BTN_STATE.borrow(cs).set( match TICKS.borrow(cs).get() {
-                    0..=9 => ButtonState::ClickedShort,
-                    _ => ButtonState::ClickedLong,
-                });
-            }
-            else{
-                TICKS.borrow(cs).set(0);
+            PRESSED.borrow(cs).set(btn.is_low().unwrap());
+            if btn.is_low().unwrap() {
+                // reset press duration
+                PRESS_DURATION.borrow(cs).set(0);
             }
         }
-            Exti::unpend(GpioLine::from_raw_line(3).unwrap());
-
+        Exti::unpend(GpioLine::from_raw_line(3).unwrap());
     });
 }
 
 #[interrupt]
-fn RTC()
-{
+fn RTC() {
     cortex_m::interrupt::free(|cs| {
         Exti::unpend(ConfigurableLine::RtcWakeup);
         WAKEUP_INT.borrow(cs).set(true)
@@ -356,12 +340,16 @@ fn RTC()
 
 #[interrupt]
 fn TIM2() {
-
     cortex_m::interrupt::free(|cs| {
         if let Some(ref mut timer) = TIMER.borrow(cs).borrow_mut().deref_mut() {
             // Clear the interrupt flag.
             timer.clear_irq();
-            TICKS.borrow(cs).set(TICKS.borrow(cs).get()+1);
+            if (PRESSED.borrow(cs).get()) {
+                PRESS_DURATION
+                    .borrow(cs)
+                    .set(PRESS_DURATION.borrow(cs).get() + 1);
+            }
+            TICKS.borrow(cs).set(TICKS.borrow(cs).get() + 1);
             PERIODIC.borrow(cs).set(true);
         }
     });
